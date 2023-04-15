45c9588fa2d206032a233d22bd794d0806b1f06e
Gavin Barham <gavinbarham@yahoo.com>
2023-04-15 13:41:09 -0500

    finish all routes and controllers for v1 spec

--- server/middleware/setFields.js
+++ server/middleware/setFields.js
@@ -1,27 +1,27 @@
 function setNutritionFields(reqObj, dbRowObj) {
     let nutritionFields = {
-    daily_steps: reqObj.dailySteps || dbRowObj.daily_steps || null,
-    first_meal_cal: reqObj.firstMealCal || dbRowObj.first_meal_cal || null,
-    first_meal_time: reqObj.firstMealTime || dbRowObj.first_meal_time || null,
-    second_meal_cal: reqObj.secondMealCal || dbRowObj.second_meal_cal || null,
-    second_meal_time: reqObj.secondMealTime || dbRowObj.second_meal_time || null,
-    snack_cal: reqObj.snackCal || dbRowObj.snack_cal || null,
-    exercise_cal: reqObj.exerciseCal || dbRowObj.exercise_cal || null,
-    exercise_time: reqObj.exerciseTime || dbRowObj.exercise_time || null
+    daily_steps: reqObj.dailySteps || dbRowObj.daily_steps,
+    first_meal_cal: reqObj.firstMealCal || dbRowObj.first_meal_cal,
+    first_meal_time: reqObj.firstMealTime || dbRowObj.first_meal_time,
+    second_meal_cal: reqObj.secondMealCal || dbRowObj.second_meal_cal,
+    second_meal_time: reqObj.secondMealTime || dbRowObj.second_meal_time,
+    snack_cal: reqObj.snackCal || dbRowObj.snack_cal,
+    exercise_cal: reqObj.exerciseCal || dbRowObj.exercise_cal,
+    exercise_time: reqObj.exerciseTime || dbRowObj.exercise_time
     }
     return nutritionFields
 }
 
 function setMedicalFields(reqObj, dbRowObj) {
     let medicalFields = {
-        oxygen: reqObj.oxygen || dbRowObj.oxygen || null,
-        heart_rate: reqObj.heartRate || dbRowObj.heart_rate || null,
-        blood_pressure: reqObj.bloodPressure || dbRowObj.blood_pressure || null,
-        blood_glucose: reqObj.bloodGlucose || dbRowObj.blood_glucose || null,
-        weight: reqObj.weight || dbRowObj.weight || null,
-        morning_meds: reqObj.morningMeds || dbRowObj.morning_meds || null,
-        noon_meds: reqObj.noonMeds || dbRowObj.noon_meds || null,
-        evening_meds: reqObj.eveningMeds || dbRowObj.evening_meds || null,
+        oxygen: reqObj.oxygen || dbRowObj.oxygen,
+        heart_rate: reqObj.heartRate || dbRowObj.heart_rate,
+        blood_pressure: reqObj.bloodPressure || dbRowObj.blood_pressure,
+        blood_glucose: reqObj.bloodGlucose || dbRowObj.blood_glucose,
+        weight: reqObj.weight || dbRowObj.weight,
+        morning_meds: reqObj.morningMeds || dbRowObj.morning_meds,
+        noon_meds: reqObj.noonMeds || dbRowObj.noon_meds,
+        evening_meds: reqObj.eveningMeds || dbRowObj.evening_meds,
     }
     return medicalFields
 }
